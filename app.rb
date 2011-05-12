7fb10dbc0e5dd00ef6927e9a16d819fa2459a929
Emerson Vinicius <talk@evinicius.com>
2011-05-11 22:20:25 -0300

    added scraper

--- app.rb
+++ app.rb
@@ -1,13 +1,50 @@
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+
+
 lib_dir = File.expand_path('lib')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
+URL_BASE = "http://groups.google.com/group/"
 
-require 'models'
 
 configure do
   set :app_file, __FILE__
-  set :haml, {:format => :html5 }
 end
 
-get "/" do
+get "/*" do
+
+  url_base = "http://groups.google.com/group/"
+  thread_ =  params["splat"]
+  thread_link  = "#{url_base}#{thread_}"
+
+  target = open("#{thread_link}").read
+
+  scraper = Nokogiri::HTML(target)
+
+  items = []
+  title = scraper.search("#thread_subject_site").text
+
+  scraper.search("#msgs div.msg").each do |msg|
+    _id =  msg.attr("id")
+    items << {  :_id => _id,   :link => "#{thread_link}##{_id}",  :title => (msg/".author span").text,  :description => (msg/".mb.cb").text,  :pub_date => (msg/"#hdn_date").attr("value")}
+  end
+
+  rss  = '<?xml version="1.0"?>'
+  rss += '<rss version="2.0">'
+  rss += "<channel><title>#{title}</title><link>#{thread_link}</link><description>XXXXX</description><pubDate>#{items[0][:pub_date]}</pubDate><lastBuildDate>#{items.reverse[0][:pub_date]}</lastBuildDate>"
+
+  items.each do |item|
+    rss += "<item>
+         <title>#{item[:title]}</title>
+         <link>#{item[:link]}</link>
+         <description>#{item[:description]}</description>
+         <pubDate>#{item[:pub_date]}</pubDate>
+         <guid>#{item[:_id]}</guid>
+      </item>"
+  end
+  rss += '</channel></rss>'
+  rss
 end
+
